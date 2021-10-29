8ab8828465b7a2b163610b1dbc431cc9266af236
Jayachandran V <jayachandran.venugopalan@bpmlinks.com>
2021-10-29 13:51:02 +0530

    Intiral 4

--- app/src/main/java/com/smf/libraryapplication/MainActivity.kt
+++ app/src/main/java/com/smf/libraryapplication/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.smf.libraryapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
 
 class MainActivity : AppCompatActivity() {
+    lateinit var editText:EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+            title = "KotlinApp"
+            editText = findViewById(R.id.editText)
+        }
+        fun saveTextFile(view: View) {
+            try {
+                val fileOutputStream: FileOutputStream = openFileOutput("mytextfile.txt", Context.MODE_PRIVATE)
+                val outputWriter = OutputStreamWriter(fileOutputStream)
+                outputWriter.write(editText.text.toString())
+                outputWriter.close()
+                //display file saved message
+                Toast.makeText(baseContext, "File saved successfully!", Toast.LENGTH_SHORT).show()
+            }
+            catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
-}
