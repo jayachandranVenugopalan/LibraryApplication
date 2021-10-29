b8a4dbc5152afbbccea52e19bf1a36b34f1b2b01
Jayachandran V <jayachandran.venugopalan@bpmlinks.com>
2021-10-29 12:40:47 +0530

    Intiral

---
+++ ApplicationLibrary/src/main/java/com/smf/applicationlibrary/MainMessage.kt
@@ -0,0 +1,12 @@
+package com.smf.applicationlibrary
+
+import android.util.Log
+
+class MainMessage {
+
+    companion object{
+        fun toast(message: String){
+            Log.d("TAG","library is called")
+        }
+    }
+}
